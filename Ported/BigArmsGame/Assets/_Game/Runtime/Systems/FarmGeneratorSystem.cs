81361c67e8a991543289c3eb7c9b71bd09420d6f
Vincent Rigaud <poonidessoo@gmail.com>
2020-02-26 10:52:49 -0500

    Add tile types

---
+++ Ported/BigArmsGame/Assets/_Game/Runtime/Systems/FarmGeneratorSystem.cs
@@ -0,0 +1,28 @@
+﻿using Unity.Entities;
+using Unity.Collections;
+using Unity.Jobs;
+
+public class FarmGeneratorSystem : JobComponentSystem
+{
+    EntityQuery queryForFarmNeedingGeneration;
+
+    public NativeArray<TileTypes> tiles;
+
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+        queryForFarmNeedingGeneration = GetEntityQuery(typeof(FarmData), typeof(FarmNeedGenerationTag));
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        // Build farm bitches
+
+        return default;
+    }
+
+    public static TileTypes GetTileType(int x, int y)
+    {
+        return TileTypes.None;
+    }
+}
