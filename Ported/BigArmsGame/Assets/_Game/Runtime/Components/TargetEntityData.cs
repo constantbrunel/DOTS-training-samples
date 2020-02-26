509cccdb9232ebb84ac5bb4fd2129c7c7e65244f
Remi Padiolleau <rpadiolleau@gmail.com>
2020-02-26 11:04:49 -0500

    Add farmer behavior selector

---
+++ Ported/BigArmsGame/Assets/_Game/Runtime/Components/TargetEntityData.cs
@@ -0,0 +1,6 @@
+﻿using Unity.Entities;
+
+public struct TargetEntityData : IComponentData
+{
+    Entity Value;
+}
---
+++ Ported/BigArmsGame/Assets/_Game/Runtime/Systems/FarmerBehavior/BehaviorSelectorSystem.cs
@@ -0,0 +1,55 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+using UnityEngine;
+
+[UpdateInGroup(typeof(SimulationSystemGroup))]
+public class BehaviorSelectorSystem : JobComponentSystem
+{
+    private EntityCommandBufferSystem m_EndSimulationSystemGroupCommandBuffer;
+
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+
+        m_EndSimulationSystemGroupCommandBuffer = World.GetExistingSystem<EndInitializationEntityCommandBufferSystem>();
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        var ecb = m_EndSimulationSystemGroupCommandBuffer.CreateCommandBuffer().ToConcurrent();
+
+        var jobHandle = Entities
+            .WithAll<FarmerTag>()
+            .ForEach((Entity entity, int entityInQueryIndex, ref FarmerBehaviorData behavior, ref DynamicBuffer<PathData> pathBuffer) =>
+            {
+                if(behavior.Value == FarmerBehavior.None)
+                {
+                    // Clear path before selecting a behavior
+                    pathBuffer.Clear();
+
+                    // Select a behavior
+                    int rand = Random.Range(0, 4);
+                    if (rand == 0)
+                    {
+                        ecb.SetComponent(entityInQueryIndex, entity, new FarmerBehaviorData() { Value = FarmerBehavior.SmashRock });
+                    }
+                    else if (rand == 1)
+                    {
+                        ecb.SetComponent(entityInQueryIndex, entity, new FarmerBehaviorData() { Value = FarmerBehavior.TillGround });
+                    }
+                    else if (rand == 2)
+                    {
+                        ecb.SetComponent(entityInQueryIndex, entity, new FarmerBehaviorData() { Value = FarmerBehavior.PlantSeed });
+                    }
+                    else if (rand == 3)
+                    {
+                        ecb.SetComponent(entityInQueryIndex, entity, new FarmerBehaviorData() { Value = FarmerBehavior.SellPlant });
+                    }
+                }
+            }).Schedule(inputDeps);
+
+        m_EndSimulationSystemGroupCommandBuffer.AddJobHandleForProducer(jobHandle);
+
+        return jobHandle;
+    }
+}
