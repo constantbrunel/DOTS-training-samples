a352f7a2088f8a0fa8309b11fbf8b83cdb5182af
Constant Brunel <cbrunel@eidosmontreal.com>
2020-02-26 14:21:20 -0500

    Plant Growing system

---
+++ Ported/BigArmsGame/Assets/_Game/Runtime/Components/PlantData.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class PlantData : MonoBehaviour, IConvertGameObjectToEntity
+{
+    public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        dstManager.AddComponentData(entity, new PlantDataComp { GrowDuration = Random.Range(2, 10) });
+        dstManager.AddComponent<IsGrowingTag>(entity);
+    }
+}
+
+public struct PlantDataComp : IComponentData
+{
+    public int PositionX;
+    public int PositionY;
+    public float Growth;
+    public int GrowDuration;
+}
---
+++ Ported/BigArmsGame/Assets/_Game/Runtime/Systems/PlantGrowthSystem.cs
@@ -0,0 +1,39 @@
+﻿using Unity.Entities;
+using Unity.Jobs;
+
+public class PlantGrowthSystem : JobComponentSystem
+{
+    private EndSimulationEntityCommandBufferSystem m_EndSimulationECBSystem;
+
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+        m_EndSimulationECBSystem = World.GetOrCreateSystem<EndSimulationEntityCommandBufferSystem>();
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        float dt = Time.DeltaTime;
+        var ecb = m_EndSimulationECBSystem.CreateCommandBuffer().ToConcurrent();
+
+        var jh = Entities.WithAll<IsGrowingTag>()
+            .ForEach((Entity entity, int entityInQueryIndex, ref PlantDataComp data) =>
+        {
+            data.Growth += dt;
+
+            // TODO - Base the scale of the plant based on the Growth/GrowDuration ratio
+            //float scale = data.Growth / (float)data.GrowDuration;
+
+            if(data.Growth >= data.GrowDuration)
+            {
+                data.Growth = data.GrowDuration;
+                ecb.RemoveComponent<IsGrowingTag>(entityInQueryIndex, entity);
+                ecb.AddComponent<IsHarvestableTag>(entityInQueryIndex, entity);
+            }
+        }).Schedule(inputDeps);
+
+        m_EndSimulationECBSystem.AddJobHandleForProducer(jh);
+
+        return jh;
+    }
+}
