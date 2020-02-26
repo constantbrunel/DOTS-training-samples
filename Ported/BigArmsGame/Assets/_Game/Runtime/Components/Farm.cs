1979a7d3e2c072cdf2b4d03afc4d66dd6ef71113
Vincent Rigaud <poonidessoo@gmail.com>
2020-02-26 10:57:44 -0500

    Add missing farm data

---
+++ Ported/BigArmsGame/Assets/_Game/Runtime/Components/Farm.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using Unity.Entities;
+using Unity.Mathematics;
+
+public class Farm : MonoBehaviour, IConvertGameObjectToEntity
+{
+	public Vector2Int MapSize;
+	public int StoreCount;
+	public int RockSpawnAttempts;
+
+	public void Convert(Entity entity, EntityManager dstManager, GameObjectConversionSystem conversionSystem)
+    {
+        dstManager.AddComponentData(entity, new FarmData()
+        {
+            MapSize = new int2(MapSize.x, MapSize.y),
+            StoreCount = StoreCount,
+            RockSpawnAttempts = RockSpawnAttempts
+        });
+        dstManager.AddComponent<FarmNeedGenerationTag>(entity);
+    }
+}
+
+public struct FarmData : IComponentData
+{
+    public int2 MapSize;
+    public int StoreCount;
+    public int RockSpawnAttempts;
+}
+
+public struct FarmNeedGenerationTag : IComponentData{}
