c4fe844e568bde16f85d1a93ff0cb163615d6799
Remi Padiolleau <rpadiolleau@gmail.com>
2020-02-28 12:12:23 -0500

    Draw tiling path and draw path data

--- Ported/BigArmsGame/Assets/_Game/Runtime/Utils/TileLimitDrawer.cs
+++ Ported/BigArmsGame/Assets/_Game/Runtime/Utils/TileLimitDrawer.cs
@@ -27,6 +27,66 @@ public class TileLimitDrawer : MonoBehaviour
             Debug.DrawLine(new Vector3(-0.5f, 0.25f, j), new Vector3(x - 0.5f, 0.25f, j));
         }
 
+        // Draw tilling zone
+        var queryTilling = entityManager.CreateEntityQuery(typeof(TillTargetData));
+        if (!queryTilling.IsEmptyIgnoreFilter)
+        {
+            var tillZones = queryTilling.ToComponentDataArray<TillTargetData>(Unity.Collections.Allocator.TempJob);
+            foreach (var tillZone in tillZones)
+            {
+                Vector3 botLeft = new Vector3(tillZone.PosX, 0.1f, tillZone.PosY);
+                Vector3 botRight = new Vector3(tillZone.PosX + tillZone.SizeX, 0.1f, tillZone.PosY);
+                Vector3 topLeft = new Vector3(tillZone.PosX, 0.1f, tillZone.PosY + tillZone.SizeY);
+                Vector3 topRight = new Vector3(tillZone.PosX + tillZone.SizeX, 0.1f, tillZone.PosY + tillZone.SizeY);
+                // Left
+                Debug.DrawLine(botLeft, topLeft, new Color(0, 1, 0));
+                // Up
+                Debug.DrawLine(topLeft, topRight, new Color(0, 1, 0));
+                // Right
+                Debug.DrawLine(topRight, botRight, new Color(0, 1, 0));
+                // Bottom
+                Debug.DrawLine(botRight, botLeft, new Color(0, 1, 0));
+            }
+            tillZones.Dispose();
+        }
+
+        // Print path for farmers
+        var queryFarmers = entityManager.CreateEntityQuery(typeof(PathData));
+        if(!queryFarmers.IsEmptyIgnoreFilter)
+        {
+            var pathDatas = queryFarmers.ToEntityArray(Unity.Collections.Allocator.TempJob);
+            foreach (var pathDataEntity in pathDatas)
+            {
+                var pathDataBuffer = entityManager.GetBuffer<PathData>(pathDataEntity);
+
+                if(pathDataBuffer.Length == 0)
+                {
+                    continue;
+                }
+
+                var posX = pathDataBuffer[0].Position.x;
+                var posY = pathDataBuffer[0].Position.y;
+
+                for(int i = 1; i < pathDataBuffer.Length; ++i)
+                {
+                    var destX = pathDataBuffer[i].Position.x;
+                    var destY = pathDataBuffer[i].Position.y;
+
+                    Vector3 from = new Vector3(posX, 0.1f, posY);
+                    Vector3 to = new Vector3(destX, 0.1f, destY);
+
+                    Debug.DrawLine(from, to, new Color(1, 0, 0));
+
+                    posX = destX;
+                    posY = destY;
+                }
+            }
+            pathDatas.Dispose();
+        }
+
+        queryFarmers.Dispose();
+        queryTilling.Dispose();
+
         farms.Dispose();
         query.Dispose();
     }
